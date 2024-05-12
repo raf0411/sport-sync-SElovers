201ef5a106f59f181d3d452703660c5db40f0f98
Raffi Darmawan <raffidarmawan0411@gmail.com>
2024-05-12 12:56:46 +0700

    nambah search bar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,14 +9,14 @@ export default function navbar() {
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Venues</a></li>
                 <li><a href="#">Community</a></li>
-            </ul>
-          
-            <div className="login-reg-container">
-                <ul>
-                    <li><button className='btn'>Login</button></li>
-                    <li><button className='btn'>Register</button></li>
-                </ul>
-            </div>
+          </ul>
+        
+          <div className="login-reg profile">
+              <ul>
+                  <li><button className='btn'>Login</button></li>
+                  <li><button className='btn'>Register</button></li>
+              </ul>
+          </div>
     </nav>
   )
 }
---
+++ src/components/search-bar/SearchBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './SearchBar.css';
+
+export default function SearchBar() {
+
+
+  return (
+      <div>
+        <input type="text" className="search-bar" />
+    </div>
+  )
+}
