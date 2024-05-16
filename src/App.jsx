16bd5d7633ed02d646ba3fa27e706627a419c9b3
Kurniawan Andhika Putra <andhikapakaya2004@gmail.com>
2024-05-16 09:32:09 +0700

    Commit pesan Anda di sini

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,28 @@
-import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Navbar from './components/navbar/Navbar.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home.jsx';
 import Venues from './pages/Venues.jsx';
 import Community from './pages/Community.jsx';
-import Group from './pages/Group.jsx';
-import Login from './pages/Login.jsx';
-import Register from './pages/Register.jsx';
-import GroupDetail from './components/group-detail/GroupDetail.jsx';
-import Navbar from './components/navbar/Navbar.jsx';
-import ErrorPage from './pages/ErrorPage.jsx';
-import Profile from './pages/Profile/Profile.jsx';
-
-const router = createBrowserRouter([
-  {
-    element: <Navbar />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/venues',
-        element: <Venues />,
-      },
-      {
-        path: '/community',
-        element: <Community />,
-      },
-      {
-        path: '/group',
-        element: <Group />,
-      },
-      {
-        path: '/group/:groupId',
-        element: <GroupDetail />,
-      },
-      {
-        path: '/login',
-        element: <Login />,
-      },
-      {
-        path: '/register',
-        element: <Register />,
-      },
-      {
-        path: '/profile',
-        element: <Profile />,
-      },
-    ]
-  }
-
-])
+import LoginRegister from './pages/LoginRegister.jsx';
 
 function App() {
 
   return (
     <>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/venues' element={<Venues />}>
+            <Route path=':venuesId'></Route>
+          </Route>
+          <Route path='/community' element={<Community />}>
+            <Route path=':communityId'></Route>
+          </Route>
+          <Route path='/login' element={<LoginRegister />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
