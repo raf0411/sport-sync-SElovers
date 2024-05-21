6283aaec201ea9a38a873461925bbd1c53a23f59
Raffi Darmawan <raffidarmawan0411@gmail.com>
2024-05-21 10:02:13 +0700

    fix responsiveness 1

--- src/components/user-post/UserPost.jsx
+++ src/components/user-post/UserPost.jsx
@@ -8,26 +8,26 @@ import profilePic from '../../assets/profile-user.png';
 export default function UserPost() {
   return (
     <div className='user-post-container'>
-        <div className="user-post">
-          <div className="post-comment">
-            <img src={profilePic} alt="Profile Pic" className="profile-pic" />
-            <input type="text" className="user-comment" placeholder='Add post...'/>
-          </div>
-          
-          <hr />
-          
-          <div className="post-img-video">
-              <button>
-                <img src={addImg} alt="Add Image" className="add-btn" />
-                <p>Photo</p>
-              </button>
-              <button>
-                <img src={addVideo} alt="Add Video" className="add-btn" />
-                <p>Video</p>
-              </button>
-              <button className='post-btn'>Post</button>
-          </div>
+      <div className="user-post">
+        <div className="post-comment">
+          <img src={profilePic} alt="Profile Pic" className="profile-pic" />
+          <input type="text" className="user-comment" placeholder='Add post...'/>
         </div>
+        
+        <hr />
+        
+        <div className="post-img-video">
+            <button>
+              <img src={addImg} alt="Add Image" className="add-btn" />
+              <p>Photo</p>
+            </button>
+            <button>
+              <img src={addVideo} alt="Add Video" className="add-btn" />
+              <p>Video</p>
+            </button>
+            <button className='post-btn'>Post</button>
+        </div>
+      </div>
     </div>
   )
 }
