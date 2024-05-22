be3cd241d87c437317dae3e3a9e26ea87d414b39
Kurniawan Andhika Putra <andhikapakaya2004@gmail.com>
2024-05-22 14:49:47 +0700

    benerin validasi register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,45 +1,55 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import '../components/login-register-component/LoginRegisterComponent.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 export default function Register() {
   const [inputs, setInputs] = useState({
-    name:"",
-    username:"",
-    email:"",
+    name: "",
+    username: "",
+    email: "",
     password: "",
   });
-
-  const [checkboxChecked, setCheckboxChecked] = useState(false);
+  const [err, setError] = useState(null);
   const [formErrors, setFormErrors] = useState({});
   const [isSubmit, setIsSubmit] = useState(false);
+  const [checkboxChecked, setCheckboxChecked] = useState(false);
 
-  const handleChange = e =>{
-    setInputs(prev=>({...prev, [e.target.name]: e.target.value}))
-  }
+  const navigate = useNavigate();
+
+  const handleChange = e => {
+    setInputs(prev => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
   const handleCheckboxChange = e => {
     setCheckboxChecked(e.target.checked);
   };
 
-  const handleSubmit = async e =>{
-    e.preventDefault()
-    try{
-      const res = await axios.post("http://localhost:8800/backend/auth/register", inputs);
-      console.log(res)
-    }catch(err){
-      console.log(err)
-    }
+  const handleSubmit = async e => {
+    e.preventDefault();
     setFormErrors(validate(inputs, checkboxChecked));
     setIsSubmit(true);
-  }
+  };
 
   useEffect(() => {
     if (Object.keys(formErrors).length === 0 && isSubmit) {
-      console.log(inputs);
+      registerUser();
     }
-  }, [formErrors])
+  }, [formErrors]);
+
+  const registerUser = async () => {
+    try {
+      const response = await axios.post("http://localhost:8800/backend/auth/register", inputs);
+      console.log(response.data);
+      navigate("/login");
+    } catch (error) {
+      if (error.response) {
+        setError(error.response.data);
+      } else {
+        setError("Registration failed. Please try again later.");
+      }
+    }
+  };
 
   const validate = (values, isChecked) => {
     const errors = {};
@@ -60,9 +70,9 @@ export default function Register() {
     if (!values.password) {
       errors.password = "Password is required!";
     } else if (values.password.length < 4) {
-      errors.password = "Password must be at least 4 characters"
+      errors.password = "Password must be at least 4 characters";
     } else if (values.password.length > 12) {
-      errors.password = "Password cannot be more than 12 characters"
+      errors.password = "Password cannot be more than 12 characters";
     }
 
     if (!isChecked) {
@@ -70,7 +80,7 @@ export default function Register() {
     }
 
     return errors;
-  }
+  };
 
   return (
     <div className='login-reg'>
@@ -78,30 +88,31 @@ export default function Register() {
         <h1>Register</h1>
 
         <div className="input-container">
-          <input type="text" className="name-input" placeholder='Your name' name='name' onChange={handleChange} value={inputs.name}/>
+          <input type="text" className="name-input" placeholder='Your name' name='name' onChange={handleChange} value={inputs.name} />
           <p className='error-text'>{formErrors.name}</p>
 
-          <input type="text" className="username-input" placeholder='Username' name='username' onChange={handleChange} value={inputs.username}/>
+          <input type="text" className="username-input" placeholder='Username' name='username' onChange={handleChange} value={inputs.username} />
           <p className='error-text'>{formErrors.username}</p>
 
-          <input type="email" className="email-input" placeholder='Email address' name='email' onChange={handleChange} value={inputs.email}/>
+          <input type="email" className="email-input" placeholder='Email address' name='email' onChange={handleChange} value={inputs.email} />
           <p className='error-text'>{formErrors.email}</p>
 
-          <input type="password" className="password-input" placeholder='Password' name='password' onChange={handleChange} value={inputs.password}/>
+          <input type="password" className="password-input" placeholder='Password' name='password' onChange={handleChange} value={inputs.password} />
           <p className='error-text'>{formErrors.password}</p>
 
         </div>
 
-        <button type='submit' onClick={handleSubmit} className='continue-btn'>Continue</button>
-        
-        {Object.keys(formErrors).length === 0 && isSubmit ? (<div className='success-message'>Account has been created!</div>) : (<></>)}
+        <button type='submit' className='continue-btn'>Continue</button>
+        {err && <p className='userexisterr'>{err}</p>}
+
+        {Object.keys(formErrors).length === 0 && isSubmit && !err && <div className='success-message'>Account has been created!</div>}
 
         <p className="create-account">
-          Already have an account? <span><Link href="" to='/login'>Login</Link></span>
+          Already have an account? <span><Link to='/login'>Login</Link></span>
         </p>
-        
+
         <div className="login-reg-agree">
-          <input type="checkbox" checked={checkboxChecked} onChange={handleCheckboxChange} required/>
+          <input type="checkbox" checked={checkboxChecked} onChange={handleCheckboxChange} required />
           <p>By continuing, I agree to the terms of use & privacy policy</p>
         </div>
 
@@ -109,5 +120,5 @@ export default function Register() {
 
       </form>
     </div>
-  )
+  );
 }
