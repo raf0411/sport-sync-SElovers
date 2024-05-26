a5ddf85646faf0cf294d60fd3cc0b5d3be38d13f
Raffi Darmawan <raffidarmawan0411@gmail.com>
2024-05-26 12:55:30 +0700

    benerin authContext

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -4,7 +4,7 @@ import { createContext, useEffect, useState } from "react";
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem("user") || null));
+    const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem("user")) || null);
     
     const login = async (inputs) => {
         const res = await axios.post("/auth/login", inputs);
