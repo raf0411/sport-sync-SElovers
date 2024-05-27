36276a40a435355e6b8546da0e43e91c97355ed8
Raffi Darmawan <raffidarmawan0411@gmail.com>
2024-05-27 12:41:48 +0700

    benerin community

--- src/components/user-post/UserPost.jsx
+++ src/components/user-post/UserPost.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './UserPost.css';
 
 import addImg from '../../assets/add-img.svg';
 import addVideo from '../../assets/add-video.svg';
 import profilePic from '../../assets/profile-user.png';
+import { AuthContext } from '../../context/authContext';
 
 export default function UserPost() {
+
+  const {currentUser} = useContext(AuthContext);
+
   return (
     <div className='user-post-container'>
       <div className="user-post">
         <div className="post-comment">
-          <img src={profilePic} alt="Profile Pic" className="profile-pic" />
+          <img src={currentUser?.profilePic} alt="Profile Pic" className="profile-pic" />
           <input type="text" className="user-comment2" placeholder='Add post...'/>
         </div>
         
